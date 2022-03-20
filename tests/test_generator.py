6b66614670cc067c4c61800dfa5584d795dc8a49
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-20 19:01:03 -0400

    Added unit tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -5,7 +5,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_generator(self):
         password = generate()
-        self.assertEqual(True, password)  # add assertion here
+        self.assertEqual(True, password)  # Should always return a random password
 
 
 if __name__ == '__main__':
