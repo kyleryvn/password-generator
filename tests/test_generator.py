c18fbd814dcb69ead409ed25dfae5828cb5afb7f
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-20 20:58:06 -0400

    Print generated password

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,8 +4,12 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_generator(self):
-        password_one = generate()
-        password_two = generate()
+        password_one = generate(16)
+        password_two = generate(16)
+
+        print(password_one)
+        print(password_two)
+
         self.assertNotEqual(password_one, password_two)  # should always return a random password
 
 
