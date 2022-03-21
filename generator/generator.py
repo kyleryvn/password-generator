16aafce0b46b46ec7b97565eb0e7c42db1306ce6
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-20 20:58:48 -0400

    Created password generation logic

--- generator/generator.py
+++ generator/generator.py
@@ -1,9 +1,22 @@
 import random
 
 
-def generate():
-    return 0
+def generate(length):
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '@', '?', '#', '$', '%', '^', '&', '*']
+    letters = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',
+               'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
+               'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z']
+
+    password_list = random.sample(numbers, 10)
+    password_list += random.sample(symbols, len(symbols))
+    password_list += random.sample(letters, len(letters))
+
+    random.shuffle(password_list)
+    password_list = random.sample(password_list, length)
+
+    return "".join(char for char in password_list)
 
 
 if __name__ == '__main__':
-    print(f'Generated password: {generate()}')
+    print(f'Generated password: {generate(16)}')
